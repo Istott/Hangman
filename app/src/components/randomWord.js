ddfcf43332103eb5af0ab99d174ec78caeabc87b
istott <isaac.cloyd@gmail.com>
2023-03-04 16:23:32 -0700

    added files back

---
+++ app/src/components/randomWord.js
@@ -0,0 +1,11 @@
+const getRandomWord = async (num) => {
+    let word = await fetch(
+      `https://random-word-api.herokuapp.com/word?length=${num}`
+    )
+      .then((response) => response.json())
+      .then((data) => data[0]);
+  
+    return word;
+  };
+  
+  export default getRandomWord;
